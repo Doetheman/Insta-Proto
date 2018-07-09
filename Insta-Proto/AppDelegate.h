96e3f2fd7aa71350d21727b81b1d795780086497
Dorian Holmes <restlessd@fb.com>
2018-07-09 14:50:40 -0700

    Initial Commit

---
+++ Insta-Proto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Insta-Proto
+//
+//  Created by Dorian Holmes on 7/9/18.
+//  Copyright © 2018 Dorian Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Insta-Proto/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Insta-Proto
+//
+//  Created by Dorian Holmes on 7/9/18.
+//  Copyright © 2018 Dorian Holmes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
